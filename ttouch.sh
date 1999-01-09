5da48fbeb430eb9ed4cf63132098d20657a932a0
Greg A. Woods <woods@robohack.ca>
1999-01-09 18:43:34 +0000

    date and time created 99/01/09 10:43:34 by woods

---
+++ ttouch.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+:
+
+i=0
+while [ "$i" -lt 10 ] ; do
+	f=`awk 'END{printf("%04d\n", '$i');}' < /dev/null`
+	touch -t 196807040343 a.$f
+	touch -t 203412251801 b.$f
+	touch 196807040343 c.$f
+	touch 203412251801 d.$f
+	i=`expr $i + 1`
+	sleep 1
+done
