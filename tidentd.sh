a2cd1d562e185c7e22524d6b80a0a3370aa2b715
Greg A. Woods <woods@robohack.ca>
2001-02-04 19:54:14 +0000

    date and time created 01/02/04 11:54:14 by woods

---
+++ tidentd.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+#
+
+if [ "$1" != sub ] ; then
+	export HOST=$(host $1 | awk '{ print $3; }')
+	declare -i I=1
+	export PPPID=$$
+
+	while [ $I -lt 1024 ] ; do
+		(
+			export I
+			socket -r -p "$0 sub" $HOST $I 2>/dev/null
+		)
+
+		I=$(($I + 1))
+	done
+else
+	LOCAL=$(netstat -n | grep ":.\+$HOST:$I.\+EST" | awk '{print $4;}' | cut -d: -f2)
+	echo "$I,$LOCAL" | nc -w 1 $HOST auth > /proc/$PPPID/fd/1
+	kill $PPID
+fi
