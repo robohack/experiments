79505cb9964cee2efe3cdd3ced5a5450cbe6579e
Greg A. Woods <woods@robohack.ca>
2001-08-02 15:36:33 +0000

    date and time created 01/08/02 08:36:33 by woods

---
+++ tmclbytes.c
@@ -0,0 +1,9 @@
+#include <sys/param.h>
+#include <sys/types.h>
+#include <stdio.h>
+main()
+{
+	printf("NBPG = %d\n", NBPG);
+	printf("CLSIZE = %d\n", CLSIZE);
+	printf("CLBYTES = %d\n", CLBYTES);
+}
