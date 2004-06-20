cdc53ab0d2597fc15c6f888553c6630a973a2a28
Greg A. Woods <woods@robohack.ca>
2004-06-20 16:39:06 +0000

    latest version

--- texecredir.sh
+++ texecredir.sh
@@ -1,14 +1,16 @@
 #! /bin/sh
 
-/usr/bin/printf 1 foo to stdout
-/usr/bin/printf 1 bar to stderr 1>&2
+/usr/bin/printf "1 foo to stdout\n"
+/usr/bin/printf "1 bar to stderr\n" 1>&2
 
 exec > /tmp/$0.$$
 
-/usr/bin/printf 2 foo to stdout
-/usr/bin/printf 2 bar to stderr 1>&2
+/usr/bin/printf "2 foo to stdout\n"
+/usr/bin/printf "2 bar to stderr\n" 1>&2
 
 exec > /dev/tty
 
-/usr/bin/printf 2 foo to stdout
-/usr/bin/printf 2 bar to stderr 1>&2
+/usr/bin/printf "3 foo to stdout\n"
+/usr/bin/printf "3 bar to stderr\n" 1>&2
+
+exit 0
