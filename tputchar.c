6bd5e781a5b195e96c0e5fe946e9f94c3c5c8f1d
Greg A. Woods <woods@robohack.ca>
2007-06-19 23:15:21 +0000

    latest version

--- tputchar.c
+++ tputchar.c
@@ -3,9 +3,21 @@
 main()
 {
 	unsigned int addr = 0x1234;
+	static const char xdigs[16] = "0123456789ABCDEF";
 
-	conputc('0' + (addr >> 16) && 0xF);
-	conputc('0' + (addr >> 12) && 0xF);
-	conputc('0' + (addr >> 8) && 0xF);
-	conputc('0' + addr && 0xF);
+	printf("0x%x=", addr);
+	putchar((int) xdigs[((addr >> 12) & 0xF)]);
+	putchar((int) xdigs[((addr >> 8) & 0xF)]);
+	putchar((int) xdigs[((addr >> 4) & 0xF)]);
+	putchar((int) xdigs[(addr & 0xF)]);
+	printf("\n");
+
+	addr = 0xFEDC;
+
+	printf("0x%x=", addr);
+	putchar((int) xdigs[((addr >> 12) & 0xF)]);
+	putchar((int) xdigs[((addr >> 8) & 0xF)]);
+	putchar((int) xdigs[((addr >> 4) & 0xF)]);
+	putchar((int) xdigs[(addr & 0xF)]);
+	printf("\n");
 }
