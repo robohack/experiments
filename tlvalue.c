14688e88cd2ab2019fef41572e78b4b1c9faf2dc
Greg A. Woods <woods@robohack.ca>
2007-07-17 21:30:15 +0000

    latest version

--- tlvalue.c
+++ tlvalue.c
@@ -1,7 +1,11 @@
-main()
+gcc_cast_as_lvalue_p()
 {
-	void *ptr = NULL;
+	void *ptr = 0;
 
-	(unsigned char *) ptr += sizeof(char);
-	
+	/* the result of a cast expression is not an lvalue! */
+#if defined(__GCC__) && (__GNUC__ <= 3)
+	(unsigned char *) ptr += 1;
+#else
+	ptr = (unsigned char *) ptr + 1;
+#endif
 }
