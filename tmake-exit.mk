06e0abb96d1cefccdd8378c3b2b02c8a6fbc9ef7
Greg A. Woods <woods@robohack.ca>
2010-03-05 18:08:05 +0000

    latest version

--- tmake-exit.mk
+++ tmake-exit.mk
@@ -1,11 +1,13 @@
 all: all-1 all-2
 
 all-1:
+	true;	\
+	false;	\
+	true;	\
+	echo "you should not see this! (but you likely will)"
+
+all-2:
 	true;
 	false;
 	true;
-
-all-2:
-	true;	\
-	false;	\
-	true;
+	echo "you should not see this either!"
