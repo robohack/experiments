9dfd2e2330cef30f48be514c210b77635e618a15
Greg A. Woods <woods@robohack.ca>
2010-04-29 02:45:59 +0000

    date and time created 10/04/28 19:45:59 by woods

---
+++ tgcc-string-concat.c
@@ -0,0 +1,7 @@
+#include <sys/cdefs.h>
+
+__COPYRIGHT("@(#) Copyright (c) 1991, 1993\
+ The Regents of the University of California.  All rights reserved.");
+
+__COPYRIGHT("@(#) Copyright (c) 1991, 1993\n"
+"	The Regents of the University of California.  All rights reserved.\n");
