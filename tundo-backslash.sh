1664a2432dead1c0ad4f7af960e8ac08d743348d
Greg A. Woods <woods@robohack.ca>
2011-03-21 19:03:20 +0000

    latest version

--- tundo-backslash.sh
+++ tundo-backslash.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 while read foo ; do
-	print "$foo"
-done < __EOF__
-This is a back \
-	slash continued line
-this is not a backslash continued line
-another back\
-slash
-another not
+	printf "%s\n" "$foo"
+done <<- '__EOF__'
+	This is a back\
+		slash continued line
+	this is not a backslash continued line
+	another back\
+	slash
+	another not
 __EOF__
