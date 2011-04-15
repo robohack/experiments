6ac4d8117717fbc628ce8ea8e2002e9954ce197f
Greg A. Woods <woods@robohack.ca>
2011-04-15 01:21:01 +0000

    date and time created 11/04/14 18:21:01 by woods

---
+++ tAF_LOCAL+SO_REUSEADDR.c
@@ -0,0 +1,49 @@
+/*
+ * $ cc -o test test.c
+ * $ ./test /tmp/foo.sock
+ * $ ./test /tmp/foo.sock
+ * test: bind(): Address already in use
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int
+main(int argc, char **argv)
+{
+	struct sockaddr_un	sun;
+	int			s, opt;
+
+	if (argc != 2)
+		errx(EXIT_FAILURE, "Usage: %s <path>", argv[0]);
+
+	if ((s = socket(PF_LOCAL, SOCK_DGRAM, 0)) == -1)
+		err(EXIT_FAILURE, "socket()");
+
+	opt = 1;
+	if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int)) == -1)
+		err(EXIT_FAILURE, "setsockopt(SO_REUSEADDR)");
+
+	sun.sun_family = AF_UNIX;
+	sun.sun_len = sizeof(sun);
+	(void)strlcpy(sun.sun_path, argv[1], sizeof(sun.sun_path));
+
+	if (bind(s, (struct sockaddr *)&sun, sun.sun_len) != 0)
+		err(EXIT_FAILURE, "bind()");
+
+	(void)close(s);
+
+	return EXIT_SUCCESS;
+}
+
+$ cc -o test test.c
+$ ./test /tmp/foo.sock
+$ ./test /tmp/foo.sock
+test: bind(): Address already in use
