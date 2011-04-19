017fe3c2f066e302603513e90f1f0fd9bbc849a9
Greg A. Woods <woods@robohack.ca>
2011-04-19 07:06:53 +0000

    date and time created 11/04/19 00:06:53 by woods

---
+++ tset.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+if [ $# = 0 ]; then
+	set --	/usr/share/dict/american \
+		/usr/share/dict/british \
+		/usr/share/dict/eign \
+		/usr/share/dict/propernames \
+		/usr/share/dict/stop \
+		/usr/share/dict/web2 \
+		/usr/share/dict/web2a \
+		/usr/share/dict/words \
+		/usr/share/dict/words.cracklib
+fi
+
+echo $*
