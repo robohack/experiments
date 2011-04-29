2d8fbe0374b1c609d80f5313b56bc22587087e57
Greg A. Woods <woods@robohack.ca>
2011-04-29 00:57:06 +0000

    date and time created 11/04/28 17:57:06 by woods

---
+++ tfindsh.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+# a script to find the correct POSIX shell pathname
+
+# Get list of paths to check
+#
+Sifs=$IFS
+IFS=:
+set $(getconf PATH)
+IFS=$Sifs
+
+# Check each path for 'sh'
+#
+for i in "$@" ; do
+	if [ -x ${i}/sh ]; then
+		Pshell=${i}/sh
+		break
+	fi
+done 
+
+echo $Pshell
