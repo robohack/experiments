a672e6ae8292f53d00b43c74efaf461d02d5804d
Greg A. Woods <woods@robohack.ca>
2011-06-19 23:24:01 +0000

    latest version

--- thugerealloc.c
+++ thugerealloc.c
@@ -1,9 +1,58 @@
+#include <limits.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+int main(void);
+
+/* this is a discovery of why a realloc() of sz bytes didn't error out */
 
 int
 main()
 {
-	size_t len, nhlen;
+	size_t len, nhlen, sz;
 
-	if (!(nh = (char *) realloc((void *) header, len + nhlen + 2))))
+	len = 14;
 
+
+	nhlen = 4294967294U;
+	printf("nhlen=%llu\n", (unsigned long long) nhlen);
+
+	sz = len + nhlen + 2;
+
+	printf("sz=%llu\n", (unsigned long long) sz);
+
+
+	nhlen = (1 << (sizeof(size_t) * CHAR_BIT)) - 2;
+	printf("nhlen=%llu\n", (unsigned long long) nhlen);
+
+	sz = len + nhlen + 2;
+
+	printf("sz=%llu\n", (unsigned long long) sz);
+
+
+	nhlen = 0 - 2;
+	printf("nhlen=%llu\n", (unsigned long long) nhlen);
+
+	sz = len + nhlen + 2;
+
+	printf("sz=%llu\n", (unsigned long long) sz);
+
+
+	nhlen = -2;
+	printf("nhlen=%llu\n", (unsigned long long) nhlen);
+
+	sz = len + nhlen + 2;
+
+	printf("sz=%llu\n", (unsigned long long) sz);
+
+
+	nhlen = ~0 - 1;			/* ~0 == -1 */
+	printf("nhlen=%llu\n", (unsigned long long) nhlen);
+
+	sz = len + nhlen + 2;
+
+	printf("sz=%llu\n", (unsigned long long) sz);
+
+
+	exit(0);
+}
