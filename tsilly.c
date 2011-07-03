ab5e63d921501ade759df39a5aeaa0642b27f2e9
Greg A. Woods <woods@robohack.ca>
2011-07-03 21:50:39 +0000

    latest version

--- tsilly.c
+++ tsilly.c
@@ -7,8 +7,13 @@ main(void)
 	p = &i;
 
 	/* insert required code here to see "10,20" output */
-	int j = 20;
-	p = &j;
+#if SUPER_SILLY
+	/* MUST be compiled with gcc on i386 WITHOUT optimization */
+	p -= 6;
+#else
+	int n = 20;
+	p = &n;
+#endif
 	/* end insert */
 
 	func();
