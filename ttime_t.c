b07e7591c78a3ef1422f205735d1918b714b9fb3
Greg A. Woods <woods@robohack.ca>
2011-07-04 01:25:25 +0000

    latest version

--- ttime_t.c
+++ ttime_t.c
@@ -1,9 +1,14 @@
+#if (__STDC_VERSION__ - 0) >= 199901L
+# include <inttypes.h>
+#endif
 #include <time.h>
 
+#include "config.h"		/* for SIZEOF_* definitions */
+
 #ifndef PRIdTIME
-# if TIME_T_MAX == INT32_MAX
-#  define PRIdTIME		PRId32
-# elif TIME_T_MAX == INT64_MAX
-#  define PRIdTIME		PRId64
+# if SIZEOF_TIME_T == SIZEOF_INT32
+#  define PRIdTIME	PRId32
+# elif SIZEOF_TIME_T == SIZEOF_INT64
+#  define PRIdTIME	PRId64
 # endif
 #endif
