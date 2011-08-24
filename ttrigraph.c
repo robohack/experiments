c2fb50ad91cf3657372219c3860df6b4c4ab13c9
Greg A. Woods <woods@robohack.ca>
2011-08-24 21:44:48 +0000

    latest version

--- ttrigraph.c
+++ ttrigraph.c
@@ -1,3 +1,8 @@
+/*
+ * see the goodness!
+ *
+ *	cc -Wall -C -CC -E ttrigraph.c
+ */
 
 #define ECHO_GUTS(N)							\
 	if (echo##N## _in  != 0) {					\
@@ -5,3 +10,14 @@
 			uart##N##_write(echo##N##_in);			\
 		echo##N##_in = 0;					\
 	}								\
+
+#define ECHO_DEFUN(N)							\
+	void								\
+	echo_##N(void)							\
+	{								\
+		ECHO_GUTS(N)						\
+	}
+
+ECHO_DEFUN(1)
+
+ECHO_DEFUN(2)
