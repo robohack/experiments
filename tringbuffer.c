c7a7173a2c2c38c425a5e1daa4f55818a8b4af9b
Greg A. Woods <woods@robohack.ca>
2011-11-04 05:21:49 +0000

    date and time created 11/11/03 22:21:49 by woods

---
+++ tringbuffer.c
@@ -0,0 +1,11 @@
+/*
+ * writing elements is Elem[WritePos % SIZE] = x; and updating the index is
+ * WritePos++; (and analogous for ReadPos). In other words, you delay the
+ * reduction modulo SIZE. For this to be efficient, you normally want to pick a
+ * power of 2 for SIZE; this makes the wrapping computation cheap and will
+ * automatically do the right thing if one of the positions ever
+ * overflows. This leads to very straightforward, efficient code. The number of
+ * items in the queue is WritePos - ReadPos; no case distinction, unsigned
+ * arithmetic does the right thing. No trouble with the last element either (if
+ * the queue is full, then "WritePos == ReadPos + SIZE" – no problem!).
+ */
