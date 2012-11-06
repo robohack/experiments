0acde532aa47e692715af145c3f4bfe1e2007d6d
Greg A. Woods <woods@robohack.ca>
2012-11-06 21:38:40 +0000

    latest version

--- tcharptrarraydiff.h
+++ tcharptrarraydiff.h
@@ -1,6 +1,7 @@
-extern char *foo;
+extern char *foo;			/* defined as "char []" */
 extern char ok[];
 extern char *str;
+extern char strarr[];			/* defined as "char *" */
 
 void bar1(char *);
-void bar2(const char *, char *);
+void bar2(const char *, void *);
