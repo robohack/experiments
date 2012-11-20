6720a5d71ffd711a1f2595d3b5c2b93723039d44
Greg A. Woods <woods@robohack.ca>
2012-11-20 11:01:52 +0000

    date and time created 12/11/20 03:01:52 by woods

---
+++ terrtrap.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+trap '
+	rc=$?;
+	if ((ERRNO > 0)); then
+		EMSG="; errno: $ERRNO"
+	else
+		EMSG=""
+	fi;
+	echo "${0#-}: last command exit code: $rc$EMSG"
+' ERR
+
+trap 'echo exit handler for $$: $? 1>&2' 0
+
+
+set -e
+echo "${0#-}: triggering failure with errexit enabled"
+false
+
+echo "why are we still here????"
+
+echo "normal exit: $rc"
+exit $rc
