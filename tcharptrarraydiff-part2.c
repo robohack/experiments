01ba55c02c07a414f8b7a5a7887187c1fca885b0
Greg A. Woods <woods@robohack.ca>
2013-07-25 21:19:34 +0000

    date and time created 13/07/25 14:19:34 by woods

---
+++ tcharptrarraydiff-part2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#if 0
+# include "tcharptrarraydiff.h"
+#else
+void bar2(const char *, void *);
+#endif
+
+/*
+ * Note that in part1 we'll declare this as:
+ *
+ *	extern char *foo;
+ */
+char foo[] = "12345678 array of chars called foo";
+char ok[] = "array of chars caleld OK";
+char *str = "a string";
+char *strarr = "annother string";
+
+extern int a;
+
+int a;
+int a;
+
+void
+bar2(const char *nm, void *f)
+{
+	printf("bar2: foo = %p\n", foo);
+	printf("bar2: &foo = %p\n", &foo);
+	printf("bar2: ok = %p\n", ok);
+	printf("bar2: str = %p\n", str);
+	printf("bar2: strarr = %p\n", strarr);
+	printf("bar2: &strarr = %p\n", &strarr);
+	printf("bar2: input: f (%s) = %p\n", nm, f);
+
+	putchar('\n');
+}
+
+/*
+ * Local Variables:
+ * eval: (make-local-variable 'compile-command)
+ * compile-command: (let ((fn (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))) (concat (default-value 'compile-command) " OPTIM=-O0 PROG=tcharptrarraydiff OBJS='tcharptrarraydiff-part1.o tcharptrarraydiff-part2.o' && ./tcharptrarraydiff"))
+ * End:
+ */
