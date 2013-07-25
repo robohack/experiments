6f898f5a74d05a41bbdb9f25c885fbd0291160f9
Greg A. Woods <woods@robohack.ca>
2013-07-25 21:06:39 +0000

    date and time created 13/07/25 14:06:39 by woods

---
+++ tfizzbuzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void);
+
+int
+main()
+{
+	int i;
+
+	printf("Clear coding:\n");
+	for (i = 1; i <= 100; i++) {
+		/* if (i % 3 == 0 && i % 5 == 0) { */
+		if (i % 15 == 0) {
+			printf("FizzBuzz\n");
+		} else if (i % 3 == 0) {
+			printf("Fizz\n");
+		} else if (i % 5 == 0) {
+			printf("Buzz\n");
+		} else {
+			printf("%d\n", i);
+		}
+	}
+
+	/*
+	 * it looks like GCC does a wee bit better with this variant, both with
+	 * and without optimization.  Note: Go doesn't have conditional exprs.
+	 */
+	printf("Idiomatic coding:\n");
+	for (i = 1; i <= 100; i++) {
+		(void) ((i % 15 == 0) ? printf("FizzBuzz\n") :
+			((i %  3 == 0) ? printf("Fizz\n") :
+			 ((i %  5 == 0) ? printf("Buzz\n") :
+			  printf("%d\n", i))));
+	}
+
+	exit(0);
+}
+
+
+/*
+ * Local Variables:
+ * eval: (make-local-variable 'compile-command)
+ * compile-command: (let ((fn (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))) (concat (default-value 'compile-command) " " fn " && ./" fn))
+ * End:
+ */
