6f1493c1163fccf5a93a489f42c78f6b78a9381f
Greg A. Woods <woods@robohack.ca>
2013-07-25 21:00:17 +0000

    date and time created 13/07/25 14:00:17 by woods

---
+++ tcppws.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define lput(str) fprintf(fp, "%s\n", str)
+
+#define M(x) x ## _
+
+#define F2	M(f2)
+
+void F2(void);
+
+F2()
+{
+	FILE	*fp;
+
+	lput("");
+	lput("foobar.");
+	lput("nutbar."                        );
+	lput("foobar.");
+}
+
+/*
+ * Local Variables:
+ * eval: (make-local-variable 'compile-command)
+ * compile-command: (let ((fn (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))) (concat (default-value 'compile-command) " " fn " && ./" fn))
+ * End:
+ */
