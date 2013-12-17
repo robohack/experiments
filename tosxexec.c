696090f9355d0597fcafd6eef10fa0e95f024cb7
Greg A. Woods <woods@robohack.ca>
2013-12-17 23:27:54 +0000

    date and time created 13/12/17 15:27:54 by woods

---
+++ tosxexec.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void);
+
+int
+main(void)
+{
+	while (1) {
+		printf("Hello world!\n");
+		sleep(1);
+	}
+}
+
+/*
+ * Local Variables:
+ * eval: (make-local-variable 'compile-command)
+ * compile-command: (let ((fn (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))) (concat (default-value 'compile-command) " " fn))
+ * End:
+ */
