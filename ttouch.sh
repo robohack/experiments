93ca6c98a9a373c09ea7591dfe8ceb4c03bc4b9c
Greg A. Woods <woods@robohack.ca>
2014-02-19 02:19:31 +0000

    latest version

--- ttouch.sh
+++ ttouch.sh
@@ -3,11 +3,11 @@
 
 i=0
 while [ "$i" -lt 10 ] ; do
-	f=`awk 'END{printf("%04d\n", '$i');}' < /dev/null`
+	f=$(awk 'END {printf("%04d\n", '$i');}' < /dev/null)
 	touch -t 196807040343 a.$f
 	touch -t 203412251801 b.$f
 	touch 196807040343 c.$f
 	touch 203412251801 d.$f
-	i=`expr $i + 1`
+	i=$(expr $i + 1)
 	sleep 1
 done
