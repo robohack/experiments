b7fa9b2ac6de4459eb723b8fb8090f9a25689145
Greg A. Woods <woods@robohack.ca>
2014-02-27 22:34:00 +0000

    latest version

--- tgcc_stmntexpr.c
+++ tgcc_stmntexpr.c
@@ -1,6 +1,11 @@
-#define MAX(a, b) (			\
+/*
+ * a variant of the "classic" GCC implementation of a CPP MAX() macro which
+ * avoids multiple expansion of its parameters by first assigning them to
+ * private variables inside a GCC "statement expression"
+ */
+#define max(a, b) (			\
 	{				\
 		__typeof__(a) a_ = (a);	\
 		__typeof__(b) b_ = (b);	\
-	      a_ > b_ ? a_ : b_;	\
+		(a_ > b_) ? a_ : b_;	\
 	})
