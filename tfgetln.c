add6b0389830b9090058213011d9e87441d4afd0
Greg A. Woods <woods@robohack.ca>
2014-06-08 01:36:54 +0000

    latest version

--- tfgetln.c
+++ tfgetln.c
@@ -1,17 +1,30 @@
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+int main(int, char *[]);
 
+int
+main(argc, argv)
+	int argc;
+	char *argv[];
+{
+	FILE *fp;
 	char *buf, *lbuf;
 	size_t len;
+
+	fp = fopen(argv[argc - 1], "r");
+	if (! fp) {
+		exit(1);
+	}
 
 	lbuf = NULL;
 	while ((buf = fgetln(fp, &len))) {
 		if (buf[len - 1] == '\n')
 			buf[len - 1] = '\0';
 		else {
-			if ((lbuf = (char *)malloc(len + 1)) == NULL)
+			if ((lbuf = (char *) malloc(len + 1)) == NULL)
 				err(1, NULL);
 			memcpy(lbuf, buf, len);
 			lbuf[len] = '\0';
@@ -24,3 +37,6 @@
 			lbuf = NULL;
 		}
 	}
+	fclose(fp);
+	exit(0);
+}
