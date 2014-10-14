03cd96b7cc3bdbde74453b20506bdbf5d7c6e63c
Greg A. Woods <woods@robohack.ca>
2014-10-14 05:51:51 +0000

    latest version

--- tatomic-optimum-init.c
+++ tatomic-optimum-init.c
@@ -1,18 +1,28 @@
+/*
+ * How to safely test for an initialized value without setting the lock first
+ *
+ * http://ridiculousfish.com/blog/posts/barrier.html
+ *
+ * http://www.cs.wustl.edu/~schmidt/editorial-3.html
+ */
+int *
 getSharedObject()
 {
-	static id sharedObject;
+	static int *sharedObject = NULL;
 
 	if (! sharedObject) {
 		LOCK;
 		if (! sharedObject) {
-			id temp = [[self alloc] init];
+			int *temp = malloc(sizeof(tmp));
 
+			init(&temp);
 			OSMemoryBarrier();
 			sharedObject = temp;
 		}
 		UNLOCK;
 	}
-	OSMemoryBarrier();
+	/* data dependency memory barrier */
+	OSMemoryBarrier();		/* not strictly necessary on PPC or x86 */
 
 	return sharedObject;
 }
