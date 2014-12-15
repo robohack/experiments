464acb4fc6fac2e8da3bd488d08aa3e4e1f9c585
Greg A. Woods <woods@robohack.ca>
2014-12-15 20:24:09 +0000

    latest version

--- tcomma.c
+++ tcomma.c
@@ -1,10 +1,18 @@
-void
+extern void boo(int, int, int);
+
+int fun(int, int, int);
+
+int
 fun(int a,
     int b,
     int c)
 {
-	
-	while ((a++ && b++ ), c--) {
+	while ((void) (a++ && b++), c--) {
 		boo(a, b, c);
 	}
-	return a
+
+	while (a++ ? 0 : b++, c--) {
+		boo(a, b, c);
+	}
+	return a;
+}
