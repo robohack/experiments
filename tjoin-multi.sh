9214ef6faa2435eff39b646d990fe5912be5e56f
Greg A. Woods <woods@robohack.ca>
2015-05-28 21:14:49 +0000

    date and time created 15/05/28 14:14:49 by woods

---
+++ tjoin-multi.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+f1=$(mktemp -t jdata)
+f2=$(mktemp -t jdata)
+
+trap "RC=$?; rm -f $f1 $f2*; exit $RC" 0 1 2 3 15
+
+# join on combined key columns #1 and #2, output only matching records
+#
+# (only 3 lines should be output, A,AB & C,CC & F,GG)
+
+sed -e 's/ //g' -e 's/,/;/' > $f1 <<__EOF__
+ADA
+ADAM
+BRUCE
+GARY
+__EOF__
+
+sed -e 's/ //g' -e 's/,/;/' > $f2 <<__EOF__
+A,AB,10
+C,CC,30
+F, GG,11
+B, XX,12
+C, DD,40
+D,DF, 4
+__EOF__
+
+sort -o $f1 $f1
+sort -o $f2 $f2
+
+# n.b.:  do not use exec -- see trap on 0 above
+
+# XXX with support for multi-column keys the external key-combining is avoided
+#join -o 1.1,1.2,1.3,2.3 -1 1,2 -2 1,2 $f1 $f2
+
+join -t , -o 1.1,1.2,2.2 -1 1 -2 1 $f1 $f2 | sed 's/;/,/'
