9dedc258354600cdeb02f70f15f22d584535cfef
Greg A. Woods <woods@robohack.ca>
2015-05-29 03:30:15 +0000

    latest version

--- tjoin-multi.sh
+++ tjoin-multi.sh
@@ -2,35 +2,37 @@
 
 f1=$(mktemp -t jdata)
 f2=$(mktemp -t jdata)
+f3=$(mktemp -t out)
 
-trap "RC=$?; rm -f $f1 $f2*; exit $RC" 0 1 2 3 15
+trap "RC=$?; rm -f $f1 $f2 $f3*; exit $RC" 0 1 2 3 15
 
-# join on combined key columns #1 and #2, output only matching records
-#
-# (only 3 lines should be output, A,AB & C,CC & F,GG)
-
-sed -e 's/ //g' -e 's/,/;/' > $f1 <<__EOF__
+sort > $f1 <<__EOF__
 ADA
 ADAM
 BRUCE
 GARY
 __EOF__
 
-sed -e 's/ //g' -e 's/,/;/' > $f2 <<__EOF__
-A,AB,10
-C,CC,30
-F, GG,11
-B, XX,12
-C, DD,40
-D,DF, 4
+tab=$(printf '\t')
+
+sort -t "${tab}" -k 1,1 > $f2 <<__EOF__
+AB${tab}1
+ABA${tab}2
+ABB${tab}3
+ADA${tab}4
+ADA${tab}5
+EVE${tab}6
+EVE${tab}7
+EVE${tab}8
+GARY${tab}9
+GARY${tab}10
 __EOF__
 
-sort -o $f1 $f1
-sort -o $f2 $f2
+cat > $f3 <<EOF
+ADA${tab}4
+ADA${tab}5
+GARY${tab}9
+GARY${tab}10
+EOF
 
-# n.b.:  do not use exec -- see trap on 0 above
-
-# XXX with support for multi-column keys the external key-combining is avoided
-#join -o 1.1,1.2,1.3,2.3 -1 1,2 -2 1,2 $f1 $f2
-
-join -t , -o 1.1,1.2,2.2 -1 1 -2 1 $f1 $f2 | sed 's/;/,/'
+join -t "${tab}" -j 1 $f1 $f2 | diff $f3 -
