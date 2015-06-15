ede1e5cfa573a7446bca4a885c36453069a155b7
Greg A. Woods <woods@robohack.ca>
2015-06-15 18:34:07 +0000

    date and time created 15/06/15 11:34:07 by woods

---
+++ tfrom-sub-shell-no-file.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+# using evan and an extra file descriptor to pass a variable values
+# from a sub-shell
+
+cmd=date
+
+set $( { ( cmd_out=$($cmd); echo "cmd_out='$cmd_out' cmd_exit=$?" >&3; ) | logger -t "$cmd"; } 3>&1 )
+
+eval $*
+
+echo "via set: $cmd='$cmd_out', exit='$cmd_exit'"
+echo ""
+
+eval $( { ( cmd2_out=$($cmd); echo "cmd2_out='$cmd2_out' cmd2_exit=$?" >&3; ) | logger -t "$cmd"; } 3>&1 )
+
+echo "via pipe: $cmd='$cmd2_out', exit='$cmd2_exit'"
+echo ""
