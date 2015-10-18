e0122caee3ac365f1d3d12fea5c960fea0454a17
Greg A. Woods <woods@robohack.ca>
2015-10-18 22:46:45 +0000

    date and time created 15/10/18 15:46:45 by woods

---
+++ tidentifiers.c
@@ -0,0 +1,47 @@
+/*
+ * typedefs, variables, and functions share the same namespace
+ *
+ * struct tags are in their own namespace
+ *
+ * note:  you still need the struct tag to provide forward declarations for
+ * anonymous struct pointers.
+ */
+
+struct s;
+//typedef struct s t;
+
+typedef struct s {
+	int x;
+} t;
+
+//int x;
+
+void x(void);
+void x()
+{
+	return;
+}
+
+//int s;
+
+void s(void);
+void s()
+{
+	return;
+}
+
+//int t;  	/* error: 't' redeclared as different kind of symbol */
+
+void t(void);	/* error: 't' redeclared as different kind of symbol */
+void t()
+{
+	return;
+}
+
+
+/*
+ * Local Variables:
+ * eval: (make-local-variable 'compile-command)
+ * compile-command: (let ((fn (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))) (concat (default-value 'compile-command) " " fn ".o"))
+ * End:
+ */
