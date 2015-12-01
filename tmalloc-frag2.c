31aab647315f7d35fb9d78d56359fe702595899c
Greg A. Woods <woods@robohack.ca>
2015-12-01 06:45:55 +0000

    latest version

--- tmalloc-frag2.c
+++ tmalloc-frag2.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_SIZE (10*1024*1024)
 
-int main (int argc, const char * argv[])
+int main(void);
+
+int
+main()
 {
 
 	size_t actual_alloc=0;
 	size_t max_alloc=0;
+	unsigned int x;
 
 	char *bigbuf=NULL;
 	size_t bigsize=0;
 
-	for  (long x=0; x<10000000; x++) {
+	for  (x = 0; x < 10000000; x++) {
 		if (bigbuf!=NULL) {
 			actual_alloc -= bigsize;
 			free(bigbuf);
@@ -27,14 +32,13 @@ int main (int argc, const char * argv[])
 			max_alloc = actual_alloc;
 		}
 		if (x%100==0) {
-			printf("alloc = %u \t max = %u\n", 
-			       (unsigned long)actual_alloc, (unsigned long)max_alloc);
+			printf("alloc = %lu \t max = %lu\n", 
+			       (unsigned long) actual_alloc, (unsigned long) max_alloc);
 
 			// max_alloc tends towards 10MB, 
 			//  "Real Mem" in activity monitor tends towards 60MB
 		}
 	}
-
 
 	exit(0);
 }
