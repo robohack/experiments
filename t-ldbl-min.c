97d402c040e4f9af000fa06c0eb875d31c936ea8
Greg A. Woods <woods@robohack.ca>
2016-01-30 05:54:41 +0000

    date and time created 16/01/29 21:54:41 by woods

---
+++ t-ldbl-min.c
@@ -0,0 +1,34 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void);
+
+int
+main()
+{
+	//char d[10] = {0x13, 0x43, 0x9b, 0x64, 0x28, 0xf8, 0xff, 0x7f, 0x00, 0x00};
+	//long double rd = *(long double*)&d;
+	long double rd = 3.3621e-4932L;
+
+	printf("DBL_MIN =\t %e (test = %e)\n", DBL_MIN, 1.0);
+	printf("LDBL_MIN =\t %Le (test = %e)\n", LDBL_MIN, 1.0);
+
+	printf("3.3621e-4932L =\t %Le (test = %e)\n", 3.3621e-4932L, 1.0);
+
+	printf("rd =\t\t %Le (test = %e)\n", rd, 1.0);		/* 3.362100e-4932 */
+
+	printf("rd * 4 =\t %Le (test = %e)\n", rd * 4.0, 1.0);
+
+	printf("3.3622e-4932L =\t %Le (test = %e)\n", 3.3622e-4932L, 1.0);
+
+	exit(0);
+}
+
+/*
+ * Local Variables:
+ * eval: (make-local-variable 'compile-command)
+ * compile-command: (let ((fn (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))) (concat (default-value 'compile-command) " " fn " && ./" fn))
+ * End:
+ */
