ff6fb5431c88efaf88451694947b7ad5ee2ef74f
Greg A. Woods <woods@robohack.ca>
2016-02-19 03:24:09 +0000

    latest version

--- treverse_list.c
+++ treverse_list.c
@@ -10,6 +10,44 @@ reverse(struct node **start)
 	*start = prev;
 }
 
+struct node *
+reverse_sll(struct node *h0)
+{
+	struct node *headOfUnReversedLL = h0;
+	struct node *headOfReversedLL = null;
+	struct node *nodeToReverse = null;
+
+	while (headOfUnReversedLL != null) {
+		headOfReversedLL = nodeToReverse;
+		nodeToReverse = headOfUnReversedLL;
+		headOfUnReversedLL = headOfUnReversedLL->next;
+		nodeToReverse->next = headOfReversedLL;
+	}
+	headOfReversedLL = nodeToReverse;
+
+	return headOfReversedLL;
+}
+
+struct node *
+recursive_reverse_sll(struct node *aNode,
+                      struct node *prevNode) /* set NULL on first call */
+{
+	struct node *nextNode = NULL;
+	struct node *headOfReversedLL = NULL;
+
+	if (aNode != NULL){
+		nextNode = aNode->next;
+		aNode->next = prevNode;
+		headOfReversedLL = recursive_reverse_sll(nextNode, aNode);
+	} else {
+		headOfReversedLL = prevNode;
+	}
+
+	return headOfReversedLL;
+}
+
+
+
 reverse_dl(struct node **start)
 {
 	struct node *curr = *start;
