4e5d457a7488f41f319fe4c11e740198a1fc52d8
Greg A. Woods <woods@robohack.ca>
2016-03-29 07:28:53 +0000

    latest version

--- trand.c
+++ trand.c
@@ -1,4 +1,15 @@
-float
-Rand_to_Float(unsigned n)
+/*
+ * The most even distribution of random numbers between 0.0 and 1.0 can be had
+ * by passing a random integer
+ */
+double
+Rand_to_Float(uintmax_t n)
 {
-	const unsigned maxRange = 1 << 24; n &= maxRange - 1; float value = n * 1.0 / maxRange; return value; } 
+	double value;
+	const uintmax_t maxRange = 1 << DBL_MANT_DIG;
+
+	n &= maxRange - 1;
+	value = n * 1.0 / maxRange;
+
+	return value;
+}
