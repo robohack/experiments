193481434dd2387e58242ae4222832dc5779d9c4
Greg A. Woods <woods@robohack.ca>
2016-05-19 02:41:59 +0000

    latest version

--- tgetcwd.c
+++ tgetcwd.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int main(void);
+
+int
 main()
 {
+	char current_dir[BUFSIZ];
 
-	printf("Working directory: %s\n", getcwd(current_dir));
+	printf("Working directory: %s\n", getcwd(current_dir, sizeof(current_dir)));
+
+	exit(0);
+}
