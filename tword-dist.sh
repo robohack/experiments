6f9e410950b7252950b3f79e8ee8960e99d9f9a9
Greg A. Woods <woods@robohack.ca>
2016-05-20 21:58:07 +0000

    latest version

--- tword-dist.sh
+++ tword-dist.sh
@@ -1,3 +1,20 @@
-for file in mail/*; do egrep -h -o '\w+' -- "$file" | awk '{print "'$file' " $0;}'; done | sort -k 2 | awk '{print $2}' | uniq -c
+#!/bin/sh
 
-for file in *??*; do egrep -h -o '\w+' -- "$file" | sed "s|^|$file\||"; done | sort -t '|' -k 2 | sed 's/^[^|]*|//' | uniq -c
+# warning:  this would be foiled by filenames with '|' chars
+
+find . -maxdepth 1 -type f -print |
+while read file; do
+    egrep -h -o "[[:alnum:]][[:alnum:]_-]*" "$file" |
+    tr [A-Z] [a-z] |
+    sed "s|^|$file\||"
+done |
+sort -t '|' -k 2 |
+uniq |
+awk -F '|' '{
+    if (lw != $2) {
+        print fc " " lw;
+        fc = 0;
+    }
+    lw = $2;
+    fc++;
+}'
