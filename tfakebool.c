60fdbac4643a9d4b8e527c8293fe44cc5b87644e
Greg A. Woods <woods@robohack.ca>
2016-06-27 21:01:56 +0000

    latest version

--- tfakebool.c
+++ tfakebool.c
@@ -1,7 +1,7 @@
 void exit(int);				/* avoid #includes */
 
-typedef int mybool;
-typedef unsigned int myubool;
+typedef int mybool;			/* this avoids problems with bool args */
+typedef unsigned int myubool;		/* this avoids problems with bool bitfields */
 
 static void
 fake_bool_test(mybool t1)
@@ -24,22 +24,33 @@ fake_ubool_test(myubool t2)
 }
 
 struct bitflags {
-	unsigned int b1:1;
-	unsigned int b2:1;
+	unsigned int ui1:1;
+	unsigned int ui2:1;
+	mybool b1:1;
+	mybool b2:1;
+	myubool ub1:1;
+	myubool ub2:1;
 };
 
 void
-mytest(int, int);
+mytest(int, unsigned int);
 
 void
 mytest(int t1,
-       int t2)
+       unsigned int t2)
 {
-	struct bitflags bf = {t1,
-	                      t2};
+	struct bitflags bf = {
+		t1,	/* clang:  warning: implicit conversion changes signedness: 'int' to 'unsigned int' [-Wsign-conversion] */
+		t2,
+		t1,
+		t2,	/* clang:  warning: implicit conversion changes signedness: 'unsigned int' to 'mybool' (aka 'int') [-Wsign-conversion] */
+		t1,	/* clang:  warning: implicit conversion changes signedness: 'int' to 'myubool' (aka 'unsigned int') [-Wsign-conversion] */
+		t2,
+	};
 
 	fake_bool_test(bf.b1);
-	fake_ubool_test(bf.b2);
+	fake_ubool_test(bf.b2);	/* gcc:  warning: passing arg 1 of `fake_ubool_test' as unsigned due to prototype */
+				/* clang: warning: implicit conversion changes signedness: 'mybool' (aka 'int') to 'myubool' (aka 'unsigned int') [-Wsign-conversion] */
 
 	return;
 }
