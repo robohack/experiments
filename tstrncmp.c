7ea09b789a8f8762a8ec635859cef0a2b932d30a
Greg A. Woods <woods@robohack.ca>
2016-09-15 00:07:43 +0000

    latest version

--- tstrncmp.c
+++ tstrncmp.c
@@ -21,13 +21,19 @@ main()
 	printf("strcmp('%s', '%s') = %d\n", s0, s0, strcmp(s0, s0)); /* s0 == s0 */
 	printf("strcmp('%s', '%s') = %d\n", s1, s0, strcmp(s1, s0)); /* s1 > s0 */
 
+	putchar('\n');
+
 	printf("strcmp('%s', '%s') = %d\n", s1, s2, strcmp(s1, s2));
 	printf("strcmp('%s', '%s') = %d\n", s1, s3, strcmp(s1, s2));
+
+	putchar('\n');
 
 	printf("strncmp('%s', '%s', %d) = %d\n", s1, s2, (int) strlen(s1), strncmp(s1, s2, strlen(s1)));
 	printf("strncmp('%s', '%s', %d) = %d\n", s1, s2, (int) strlen(s2), strncmp(s1, s2, strlen(s2)));
 	printf("strncmp('%s', '%s', %d) = %d\n", s1, s3, (int) strlen(s1), strncmp(s1, s3, strlen(s1)));
 	printf("strncmp('%s', '%s', %d) = %d\n", s1, s3, (int) strlen(s3), strncmp(s1, s3, strlen(s2)));
+
+	putchar('\n');
 
 	s0 = "010029";
 	s1 = "010030";
@@ -35,6 +41,17 @@ main()
 	printf("strcmp('%s', '%s') = %d\n", s0, s1, strcmp(s0, s1)); /* s0 < s1 */
 	printf("strcmp('%s', '%s') = %d\n", s0, s0, strcmp(s0, s0)); /* s0 == s0 */
 	printf("strcmp('%s', '%s') = %d\n", s1, s0, strcmp(s1, s0)); /* s1 > s0 */
+
+	putchar('\n');
+
+	s0 = "0100";
+	s1 = "010030";
+
+	printf("strcmp('%s', '%s') = %d\n", s0, s1, strcmp(s0, s1)); /* s0 < s1 */
+	printf("strcmp('%s', '%s') = %d\n", s0, s0, strcmp(s0, s0)); /* s0 == s0 */
+	printf("strcmp('%s', '%s') = %d\n", s1, s0, strcmp(s1, s0)); /* s1 > s0 */
+
+	putchar('\n');
 
 	s0 = "010030-rc2";
 	s1 = "010030";
--- tswap.h
+++ tswap.h
@@ -1,9 +1,30 @@
+
+/*
+ * using statement expressions and typeof() to implement a stack-based swap,
+ * and doing so without shadowing a usable symbol
+ */
+#define SWAP_GCC(a, b)	({ typeof(a) SWAP_GCC = a; a = b; b = SWAP_GCC; })
+
+
+/*
+ * A somewhat more portable alternative:
+ *
+ * N.B.:  the ternary expression assures that 'a' and 'b' have the same type
+ */
+#define SWAP(a, b)	__swap_internal(&(a), &(b), sizeof(*(1 ? &(a) : &(b))))
+
 #include <string.h>
 
-#define SWAP(a, b) swap_internal(&(a), &(b), sizeof *(1 ? &(a) : &(b)))
-static inline void swap_internal(void *a, void *b, size_t size) {
-    char tmp[size];
-    memcpy(tmp, a, size);
-    memmove(a, b, size);
-    memcpy(b, tmp, size);
+static inline void
+__swap_internal(void *a,
+                void *b,
+                size_t size)
+{
+	char tmp[size];
+
+	memcpy(tmp, a, size);
+	memmove(a, b, size);
+	memcpy(b, tmp, size);
+
+	return;
 }
