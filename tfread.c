2f80736d7fe2635bead156ca48521d1084c08481
Greg A. Woods <woods@robohack.ca>
2016-11-23 01:34:13 +0000

    date and time created 16/11/22 17:34:13 by woods

---
+++ tfread.c
@@ -0,0 +1,58 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int, char *[]);
+
+int
+main(argc, argv)
+	int argc;
+	char *argv[];
+{
+	const char *fn;
+	FILE *fp;
+	char buf[BUFSIZ];
+	ssize_t nr;
+	int res;
+
+	if (argc == 1 || argv[1] == NULL) {
+		fp = stdin;
+		fn = "stdin";
+	} else if (argc == 2) {
+		fn = argv[1];
+		if ((fp = fopen(fn, "r")) == NULL) {
+			perror(fn);
+			exit(1);
+		}
+	} else {
+		fprintf(stderr, "Usage: %s [file]\n", argv[0]);
+		exit(1);
+	}
+#if 1
+	res = fcntl(fileno(fp), F_SETFL, O_NONBLOCK);
+	if (res != 0) {
+		fprintf(stderr, "%s: fcntl: %s\n", fn, strerror(errno));
+		exit(1);
+	}
+#endif
+
+	while ((nr = fread(buf, (size_t) 1, (size_t) 100, fp)) > 0) {
+		if (nr < 100) {
+			fprintf(stderr, "%s: read was %jd bytes short of %d)\n", fn, (intmax_t) 100 - nr, 100);
+		}
+	}
+
+	if (!feof(fp)) {
+		fprintf(stderr, "%s: returned before EOF (read %jd of %d)\n", fn, (intmax_t) nr, 100);
+	}
+	if (ferror(fp)) {
+		perror(fn);
+		exit(1);
+	}
+
+	exit(0);
+}
