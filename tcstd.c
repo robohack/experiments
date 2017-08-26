5b1dddbd099e2f0cae27fdf0c344b9c803b7af24
Greg A. Woods <woods@robohack.ca>
2017-08-26 03:17:48 +0000

    latest version

--- tcstd.c
+++ tcstd.c
@@ -7,16 +7,26 @@
 int main(void);
 #endif
 
+/* based on:  https://stackoverflow.com/q/31115453/816536 */
+
 int
 main()
 {
-	printf("C%d\n",(int)(90-(-4.5//**/
-	                         -4.5)));
+	printf("C%d\n", (int) (90 - (-4.5//* fooled you! */
+				     -4.5)));
+#ifndef __STDC__
+	puts("Errr... actually K&R");
+#endif
+#if (__STDC_VERSION__ - 0) >= 201112L
+	puts("Errr... actually C11");
+#endif
 
 	exit(0);
 }
 
 /* XXX compile with -std=c89 to write "C89", -std=c99 to write "C99" */
+
+/* xxx in theory with a really old GCC one could use '-traditional' for K&R */
 
 /*
  * Local Variables:
