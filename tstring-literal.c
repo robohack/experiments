e1568a2e3e7a5814c8c5c73fc0718e4ebaf93338
Greg A. Woods <woods@robohack.ca>
2017-09-02 01:15:53 +0000

    date and time created 17/09/01 18:15:53 by woods

---
+++ tstring-literal.c
@@ -0,0 +1,58 @@
+const char *gstring =
+#ifdef UNIQUE
+	"1 "
+#endif
+	"this is a literal string";
+char garray[] =
+#ifdef UNIQUE
+	"2 "
+#endif
+	"this is a literal string";
+
+int
+bar(int x,
+    int y)
+{
+	const char *fstring =
+#ifdef UNIQUE
+		"3 "
+#endif
+		"this is a literal string";
+	char farray[] =
+#ifdef UNIQUE
+		"4 "
+#endif
+		"this is a literal string";
+	static const char *sfstring =
+#ifdef UNIQUE
+		"5 "
+#endif
+		"this is a literal string";
+	static char sfarray[] =
+#ifdef UNIQUE
+		"6 "
+#endif
+		"this is a literal string";
+
+	switch (x) {
+	case 0:
+		return fstring[y];
+	case 1:
+		return farray[y];
+	case 2:
+		return sfstring[y];
+	case 3:
+		return sfarray[y];
+	default:
+		return farray[y];
+	}
+}
+
+/*
+ * add -DUNIQUE to force each string to be unique
+ *
+ * Local Variables:
+ * eval: (make-local-variable 'compile-command)
+ * compile-command: (let ((fn (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))) (concat "cc -O2 -S -c " fn ".c && cat " fn ".s"))
+ * End:
+ */
