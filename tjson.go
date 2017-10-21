bf2220fb9d28b4602ae7f40ecbebc4c5e3ef6079
Greg A. Woods <woods@robohack.ca>
2017-10-21 00:07:30 +0000

    latest version

--- tjson.go
+++ tjson.go
@@ -1,3 +1,11 @@
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Response1 struct {
     Page   int
     Fruits []string
@@ -16,6 +24,13 @@ func demoStructs() {
 	fmt.Println(string(res1B))
 	fmt.Println("")
 
+	fancy, err := json.MarshalIndent(res1D, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(fancy))
+	fmt.Println("")
+
 	res2D := &Response2 {
 		Page:   1,
 		Fruits: []string{"apple", "peach", "pear"}}
@@ -24,11 +39,11 @@ func demoStructs() {
 	fmt.Println(string(res2B))
 	fmt.Println("")
 
-	fancy, err := json.MarshalIndent(res2B, "", "    ")
+	fancy2, err := json.MarshalIndent(res2D, "", "\t")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(fancy))
+	fmt.Println(string(fancy2))
 	fmt.Println("")
 
 	str := `{"sheet": 1, "food": ["apple", "peach"]}`
@@ -38,3 +53,7 @@ func demoStructs() {
 	fmt.Println(res.Fruits[0])
 	fmt.Println("")
 }
+
+func main() {
+	demoStructs()
+}	
