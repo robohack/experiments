7f06f642058b8fca31cce5f4f988856b2012fb6e
Greg A. Woods <woods@robohack.ca>
2017-11-03 06:25:54 +0000

    latest version

--- tcanonicalize_path.sh
+++ tcanonicalize_path.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
-TARGET_FILE=$1
+TARGET_FILE="$1"
 
-cd $(dirname $TARGET_FILE)
-TARGET_FILE=$(basename $TARGET_FILE)
+# XXX command subst vs. splitting?
+
+TARGET_DIR="$(dirname "${TARGET_FILE}")"
+cd "${TARGET_DIR}"
+
+TARGET_FILE="$(basename "${TARGET_FILE}")"
 
 # Iterate down the path through a (possible) chain of symlinks
 #
+# XXX check for symlink loops by watching for too many iterations
+#
 while [ -L "${TARGET_FILE}" ]; do
-	TARGET_FILE=$(readlink ${TARGET_FILE})
-	cd $(dirname ${TARGET_FILE})
-	TARGET_FILE=$(basename ${TARGET_FILE})
+	TARGET_FILE="$(readlink "${TARGET_FILE}")"
+	cd "$(dirname "${TARGET_FILE}")"
+	TARGET_FILE="$(basename "${TARGET_FILE}")"
 done
 
-# Compute the canonicalized name by finding the physical path
-# for the directory we're in and appending the target file.
+# Compute the canonicalized name by asking the kernel for the physical path of
+# the directory we're now in and appending the target file.
 #
-PHYS_DIR=$(pwd -P)
-RESULT=${PHYS_DIR}/${TARGET_FILE}
-echo $RESULT
+PHYS_DIR="$(pwd -P)"
+RESULT="${PHYS_DIR}/${TARGET_FILE}"
+
+echo "${RESULT}"
