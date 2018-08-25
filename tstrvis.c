3807634c590fc02f16a5ef1c297e635b3022d6c6
Greg A. Woods <woods@robohack.ca>
2018-08-25 05:19:00 +0000

    latest version

--- tstrvis.c
+++ tstrvis.c
@@ -1,12 +1,25 @@
 #include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <vis.h>
 
+int main(void);
+
+int
 main()
 {
 	char foo[100];
 
 	strvis(foo, "\\\"\abcdef", VIS_CSTYLE);
 	puts(foo);
+
+	strvis(foo, "this is percent(%)F:  %F", VIS_TAB|VIS_NL);
+	puts(foo);
+
+	strvis(foo, "date > $HOME/tmp/$(TZ=UTC0 date +\%FT\%TZ).date", VIS_TAB|VIS_NL);
+	puts(foo);
+
+	exit(0);
 }
 
 /*
