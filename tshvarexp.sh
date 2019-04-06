7b67224394d51457948e13e9f32b52872d664a06
Greg A. Woods <woods@robohack.ca>
2019-04-06 19:40:21 +0000

    latest version

--- tshvarexp.sh
+++ tshvarexp.sh
@@ -1,6 +1,62 @@
 #!/bin/sh
 
 DATE=date
+DATER="${DATE} -r 0"
+CAT=cat
+CATV="${CAT} -v"
 
-echo "${DATE}"
+ISO8601='%Y-%m-%dT%H:%M:%SZ'
+TAB=$(printf '\t')
 
+# Lesson:  Don't put code in a variable -- put it in a function!
+DATEALL ()
+{
+	${DATE} -r 0 "+foo${TAB}${ISO8601}${TAB}none"
+}
+echo "plain '${DATE}' and ${DATER}"
+
+TZ=0
+export TZ
+
+${DATE}
+${DATER}
+${DATER} +"foo${TAB}${ISO8601}${TAB}none"
+DATEALL
+
+echo "through a pipe to cat"
+${DATE} | ${CAT}
+${DATER} | ${CAT}
+${DATER} | ${CATV}
+${DATER} \
+	+"foo${TAB}${ISO8601}${TAB}none" | \
+	${CAT}
+DATEALL | ${CAT}
+
+echo "through a pipe to while"
+${DATE} | while read txt; do
+	echo "Read: ${txt}"
+	done
+${DATER} | while read txt; do
+	echo "Read: ${txt}"
+	done
+${DATER} +"foo${TAB}${ISO8601}${TAB}none" | \
+	while read foo txt none; do
+		if [ "${foo}" != "foo" ]; then
+			echo "read: field splitting failed: '${foo}' != 'foo'"
+		fi
+		if [ "${none}" != "none" ]; then
+			echo "read: field splitting failed: '${none}' != 'none'"
+		fi
+		echo "Read: '${txt}'"
+	done
+
+DATEALL | \
+	while read foo txt none; do
+		if [ "${foo}" != "foo" ]; then
+			echo "read: field splitting failed: '${foo}' != 'foo'"
+		fi
+		if [ "${none}" != "none" ]; then
+			echo "read: field splitting failed: '${none}' != 'none'"
+		fi
+		echo "Read: '${txt}'"
+	done
