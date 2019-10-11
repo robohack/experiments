935d5c9e657dca4b04959d718677b1567e9aca4f
Greg A. Woods <woods@robohack.ca>
2019-10-11 17:19:58 +0000

    latest version

--- tmacro-block.c
+++ tmacro-block.c
@@ -2,3 +2,30 @@
  * try to understand why "do {} while(0)" might be preferred to any other form
  * of block for a macro with multiple statements that behaves like a function.
  */
+#define dwb(p)								\
+	do {								\
+		int bar = 1;						\
+		if (bar > (p)) {					\
+			printf("smaller\n");				\
+		}							\
+		printf("p = %d\n", p);					\
+	} while(0)
+
+#define ifb(p)								\
+	if (1) {							\
+		int bar = 1;						\
+		if (bar > (p)) {					\
+			printf("smaller\n");				\
+		}							\
+		printf("p = %d\n", p);					\
+	}
+
+#define pb(p)								\
+	{								\
+		int bar = 1;						\
+		if (bar > (p)) {					\
+			printf("smaller\n");				\
+		}							\
+		printf("p = %d\n", p);					\
+	}
+
