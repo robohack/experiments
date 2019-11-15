314064f0e65d8c0523855733a83a30756fc2a566
Greg A. Woods <woods@robohack.ca>
2019-11-15 21:10:31 +0000

    latest version

--- tmake-bang.mk
+++ tmake-bang.mk
@@ -1,6 +1,23 @@
-foo!= echo bar#baz
+#
+# tests related to http://austingroupbugs.net/view.php?id=337
+#
+
+bar != echo bar#baz
+
+# Presume ASCII or UTF-8:
+hash != printf '\043'
+barbaz != echo bar$(hash)baz
+
+XYZZY = fiddle-dee-dee
+dollar = $$
+VAR3 != printf '%s' '$(dollar)(XYZZY)'
+GVAR3 = $(shell printf '%s' '$(dollar)XYZZY')
+
 all:
-	echo '<${foo}>'
+	echo 'bar=<${bar}>'
+	echo 'barbaz=<${barbaz}>'
+	@printf '%s\n' 'VAR3=<$(VAR3)>'
+	@printf '%s\n' 'GVAR3=<$(GVAR3)>'
 #
 # Local Variables:
 # eval: (make-local-variable 'compile-command)
