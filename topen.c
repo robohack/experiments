fa460ade3efb7be3c6e382bf0f62adeebfde7ffe
Greg A. Woods <woods@robohack.ca>
2022-01-04 16:24:19 -0800

    topen.c: make this work again

--- topen.c
+++ topen.c
@@ -1,11 +1,22 @@
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-main(argc, argv)
-	int argc;
-	char *argv[];
+int main(void);
+
+int
+main()
 {
 	if (open("foo.open", O_WRONLY | O_APPEND, 0644) < 0)
 		perror("foo.open");
 
 	exit(0);
 }
+
+
+/*
+ * Local Variables:
+ * eval: (make-local-variable 'compile-command)
+ * compile-command: (let ((fn (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))) (concat "rm " fn "; " (default-value 'compile-command) " " fn " && ./" fn))
+ * End:
+ */
