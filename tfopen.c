3acb3cc3907c610a413d455476337b125cf89ca3
Greg A. Woods <woods@robohack.ca>
2022-01-04 16:23:43 -0800

    tfopen.c: add tests for __sflags() internal helper

--- tfopen.c
+++ tfopen.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int __sflags(const char *mode, int *optr);
+
+int main(int, char *[]);
+
+int
 main(argc, argv)
 	int argc;
 	char *argv[];
 {
-	if (fopen(argv[1], "r") < 0)
-		perror(argv[1]);
+	int r;
+	int mode;
+
+	(void) argc;
+
+	r = __sflags("re+", &mode);
+	printf("<fcntl.h>: 're+' =\t(0x%02x) 0x%x\n", r, mode);
+	r = __sflags("we+", &mode);
+	printf("<fcntl.h>: 'we+' =\t(0x%02x) 0x%x\n", r, mode);
+	r = __sflags("re", &mode);
+	printf("<fcntl.h>: 're' =\t(0x%02x) 0x%x\n", r, mode);
+
+	if (fopen(argv[1], "r") == NULL)
+		perror(argv[1] ? argv[1] : "(null)");
 
 	exit(0);
 }
+
+
+/*
+ * Local Variables:
+ * eval: (make-local-variable 'compile-command)
+ * compile-command: (let ((fn (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))) (concat "rm " fn "; " (default-value 'compile-command) " " fn " && ./" fn))
+ * End:
+ */
