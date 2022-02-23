0d91b18403c70b630c9b7a0061405cb1b715eb8c
Greg A. Woods <woods@robohack.ca>
2022-02-22 17:07:23 -0800

    tfunc.sh, tlocal.sh:  improvements and shuffling about
    - move the "local" tests entirely to tlocal.sh
    - fix detection of at&t ksh
    - fix detection of "local" builtin
    - make definition of fake "local" function safe from dash
    - add an example of runtime dynamic function definition to tfunc.sh

--- tfunc.sh
+++ tfunc.sh
@@ -1,61 +1,31 @@
 #!/bin/sh
 #
-# test shell functions
+# test shell function definition
 #
 
-bar=none
-ebar=enone
-export ebar
-lbar=nope
+echo ""
+echo "defun_func_test:"
 
-# every truly POSIX-compliant shell implements the built-in
-# command 'local', but then there's ksh (even #93!)
-#
-if type local >/dev/null 2>&1; then
-	: a decent shell
-else
-	# Ksh93:
-	#
-	#	typeset vname[=value]
-	#
-	#	When invoked inside a function defined with the
-	#	function name syntax, a new instance of the variable
-	#	'vname' is created, and the variable's value and type
-	#	are restored when the function completes.
-	#
-	# sadly this does not work for functions defined as func() {}
-	# (and that's the only portable way to define a function!)
-	#
-	# I used to like Ksh -- now I'm glad it's only an inspiration...
-	#
-	alias local='typeset'
-fi
-
-testit ()
+defun ()
 {
-	local lbar
+	_param=$1
+	# N.B.:  quoting is HARD!
+	_defun_newfunc="newfunc () { echo ${_param}: 'this is from newfunc!'  \"argv[1]=\$1\"; }"
+	eval ${_defun_newfunc}
+	unset _defun_newfunc _param
 
-	bar=some
-	ebar=esome
-	lbar=lsome
+	echo "from_inside:"
+	newfunc INSIDE
 }
 
-echo bar=$bar
-echo ebar=$ebar
-echo lbar=$lbar
-testit
-if [ $bar != some ]; then
-	echo OOPS!!! $bar=$bar
-else
-	echo bar=$bar
-fi
-if [ $ebar != esome ]; then
-	echo OOPS!!! $ebar=$ebar
-else
-	echo ebar=$ebar
-fi
-if [ $lbar = lsome ]; then
-	echo OOPS!!! $lbar=$lbar
-else
-	echo lbar=$lbar
-fi
+defun HELLO
+
+echo "from_outside:"
+newfunc OUTSIDE
+
+# Change the leading "sh" to whatever other shell you want to test...
+#
+# Local Variables:
+# eval: (make-local-variable 'compile-command)
+# compile-command: (concat "sh ./" (file-name-nondirectory (buffer-file-name)))
+# End:
--- tlocal.sh
+++ tlocal.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-is_attksh()
+is_bash ()
 {
-	# N.B.:  we cannot use 'local' for these variables
-	# (xxx well, we could if we tested for 'local' and 'typeset')
+	[ ${RANDOM:-0} -ne ${RANDOM:-0} -a -n "${BASH}" ]
+}
+is_bash && echo "Bash: ${BASH_VERSION}" || echo "not bash"
 
-	# ksh93
+is_attksh ()
+{
+	is_bash && return 1
+
+	# ksh93 and [p]bosh
 	_sh_version=`eval 'echo "${.sh.version}"' 2>/dev/null`
-	# pdksh only
-	_opt_login=`set -o 2>/dev/null | grep login`
+	# [p]bosh only
+	_sh_shell=`eval 'echo "${.sh.shell}"' 2>/dev/null`
 
-	test -n "${_sh_version}" -o \( -z "${_opt_login}" -a -n "${_}" -a -n "${ERRNO}" -a -n "${FCEDIT}" -a -n "${PS3}" \)
+	test -n "${_sh_version}" -a -z "${_sh_shell}"
+	_rc=$?
+	unset _sh_shell _sh_version
+
+	return ${_rc}
 }
 is_attksh && echo "AT&T Ksh${_sh_version:+: }${_sh_version:- (probably ksh88 or ksh86)}" || echo "not real ksh"
 
@@ -20,29 +29,41 @@ is_zsh()
 }
 is_zsh && echo "Zsh: ${ZSH_VERSION}" || echo "not zsh"
 
-# AT&T Ksh is (still, as of ksh93u+) the only shell I know of which has
-# functions, and which has a 'typset' command, but which does not have a 'local'
-# command to declare function-scope variables, nor a "built-in" alias for it
-# either.  Sigh.
+
+defun_local="local () { :; }"
+
+# every truly POSIX-compliant shell implements the built-in
+# command 'local', but then there's ksh (even #93!)
 #
-if is_attksh; then
-	# not that this has the desired effect with AT&T ksh, since we don't
-	# currently use the "function foo {}" syntax for defining functions, and
-	# to do that we would have to somehow elide/ignore 'function' for other
-	# shells which don't offer this syntax.
+# XXX the POSIX version of bosh (pbosh) fails this test and doesn't have typeset
+#
+if is_attksh; then		# if type local >/dev/null 2>&1; then
 	#
-	# XXX so don't shadow globals in functions!!!
-
-	# xxx strictly speaking this probably should be a function that would
-	# treat any use of options as a usage error, BUT even though this is in
-	# an 'if' that wouldn't be executed by it, some versions of 'dash' will
-	# barf on having a function definition trying to re-define the 'local'
-	# builtin here!!!  (dash-0.5.5.1 is OK, dash-0.5.9.1 is stupid)
-
-	alias local=typeset
+	# AT&T Ksh93:
+	#
+	#	typeset vname[=value]
+	#
+	#	When invoked inside a function defined with the
+	#	function name syntax, a new instance of the variable
+	#	'vname' is created, and the variable's value and type
+	#	are restored when the function completes.
+	#
+	# XXX Sadly this does not actually work for functions defined using the
+	# "func() {body}" syntax, and that's the only portable way to define a
+	# function!
+	#
+	# XXX so don't shadow globals in "func(){}" functions!!!
+	#
+	# Note that in AT&T Ksh (and others?) aliases are resolved when a
+	# function definition is first read in, so set this early!
+	#
+	# I used to like AT&T Ksh -- now I'm glad it's only an inspiration...
+	#
+	alias local='typeset'
 else
 	# N.B.:  ancient "ash" had no 'type' builtin, though I implemented one
 	# long ago.  It is in ~/.ashtype, with this to load it from ~/.profile
+	# (see github.com/robohack/dotfiles)
 
 	# the I/O re-direction doesn't actually get rid of the "type: not found" message
 	# from the old Ash implementation...  Perhaps it parses the whole line first?
@@ -56,15 +77,67 @@ else
 		. ${HOME}/.ashtype
 	fi
 
-	# for Bourne (heirloom) sh.
-	if expr "`type local 2>/dev/null`" : 'local not found$' > /dev/null 2>&1 ; then
-		# XXX see above about dash-0.5.9.1 stupidity
-		local()
-		{
-			:
-		}
+	if type local >/dev/null 2>&1; then
+		:
+	else
+		# for Bourne (heirloom) sh, pbosh, etc.(?)
+		#
+		# This definition of "local" won't actually do anything useful,
+		# but it also won't cause a syntax error either.
+		#
+		# xxx even though this is in an 'if' that souldn't be executed
+		# by it, some versions of 'dash', aka "Debian Almquist shell"
+		# (which started life as NetBSD sh) will barf and abort just
+		# seeing this function definition for 'local' here!!!
+		# (dash-0.5.5.1 is OK, dash-0.5.9.1 through 0.5.10.2 are STUPID)
+		#
+		# even worse it is almost impossible to programatically detect
+		# dash programatically.  However luckily we can hide it in a
+		# variable value....
+		#
+		eval ${defun_local}
 	fi
 fi
+
+bar=none
+ebar=enone
+export ebar
+lbar=nope
+
+testit ()
+{
+	local lbar
+
+	bar=some
+	ebar=esome
+	lbar=lsome
+}
+
+echo "before:"
+echo bar=$bar
+echo ebar=$ebar
+echo lbar=$lbar
+
+testit
+
+echo "after:"
+if [ $bar != some ]; then
+	echo ERROR:  OOPS!!! bar=$bar
+else
+	echo bar=$bar
+fi
+if [ $ebar != esome ]; then
+	echo ERROR:  OOPS!!! ebar=$ebar
+else
+	echo ebar=$ebar
+fi
+if [ $lbar = lsome ]; then
+	echo ERROR:  OOPS!!! lbar=$lbar
+else
+	echo lbar=$lbar
+fi
+
+echo ""
 
 tvar=foo
 
@@ -79,19 +152,29 @@ tfun
 echo tvar=${tvar}
 
 # XXX this is really disturbing!!  It is not safe to put the opening paren on
-# its own line as that would lead any non-AT&T-Ksh shell to just say "function:
-# not found" and then execute the function body immediately as if it were a
-# normal compound command AND THEN CONTINUE RUNNING THE SCRIPT!!!
+# its own line as that would lead many non-AT&T-Ksh shells to just say
+# "function:  not found" and then execute the function body immediately as if it
+# were a normal compound command AND THEN CONTINUE RUNNING THE SCRIPT!!!
 #
-# at least with the opening paren up on the definition line, any non-AT&T-Ksh
-# (i.e. not fully compatible) shell will stop (after executing the function body
-# as individual commands) with a syntax error at the closing paren.
+# Some though, like "bosh" and "dash", complain and die when seeing "local"
+# oustide of a function so don't continue and run the script body.
 #
-function tfun2 {
+# at least with the opening paren up on the definition line, those non-AT&T-Ksh
+# (i.e. not fully compatible) shells like "sh" and "pbosh" will stop (after
+# executing the function body) with a syntax error at the closing paren.
+#
+function tfun2	{
 	local tvar
 	tvar=none
 	echo "Hi from tfun2!  tvar=${tvar}"
-}
+}				# xxx non-Ksh/bash barfs here!
 tfun2
 
 echo tvar=${tvar}
+
+# Change the leading "sh" to whatever other shell you want to test...
+#
+# Local Variables:
+# eval: (make-local-variable 'compile-command)
+# compile-command: (concat "sh ./" (file-name-nondirectory (buffer-file-name)))
+# End:
