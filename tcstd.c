feb029ecd72a010014fe5feab29aa980c354fe42
Greg A. Woods <woods@robohack.ca>
2023-07-15 16:18:54 -0700

    tcstd.c: add more informative output

--- tcstd.c
+++ tcstd.c
@@ -16,9 +16,24 @@ main()
 				     -4.5)));
 #ifndef __STDC__
 	puts("Errr... actually K&R");
+#else
+	printf("__STDC__ = %ld\n", (long) __STDC__);
 #endif
-#if (__STDC_VERSION__ - 0) >= 201112L
+
+#ifdef __STDC_VERSION__
+	printf("__STDC_VERSION__ = %ld\n", (long) __STDC_VERSION__);
+# if (__STDC_VERSION__ - 0) >= 201112L && (__STDC_VERSION__ - 0) < 201700L
 	puts("Errr... actually C11");
+# elif (__STDC_VERSION__ - 0) >= 201700L && (__STDC_VERSION__ - 0) < 202000L
+	puts("Errr... actually C17");
+# elif (__STDC_VERSION__ - 0) >= 202000L
+	puts("Errr... actually C2x");
+# endif
+#endif
+
+#ifdef __GNUC__
+	printf("__GNUC__ = %ld\n", (long) __GNUC__);
+	puts("Unfortunately GCC always supports its extensions\n-- use '-pedantic' to get warnings when/where they are used");
 #endif
 
 	exit(0);
