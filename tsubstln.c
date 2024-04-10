0567dc4ebf0bf0db2fe19eed76c12a7382b7ef12
Greg A. Woods <woods@robohack.ca>
2024-04-10 12:40:49 -0700

    tsubstln.c: also check ferror() at EOF on last line

--- tsubstln.c
+++ tsubstln.c
@@ -30,30 +30,35 @@ main(int argc, char *argv[])
 	 * storage can be successfully allocated, or else it will return NULL.
 	 *
 	 * The resulting array may either end with a newline, or if EOF was
-	 * reached then no newline`, so we can assume EOF if there is no newline
+	 * reached then no newline, so we can assume EOF if there is no newline
 	 *
-	 * note:  POSIX (IEEE 1003.1-2008) now has a getline(3) function, but
-	 * it has a somewhat different API...
+	 * note:  POSIX (IEEE 1003.1-2008) now has a getline(3) function (and
+	 * the more flexible getdelim(3)), but it(they) has a somewhat different
+	 * API...
 	 *
 	 *	char *ln = NULL;
 	 *	size_t lnsz = 0;
 	 *	ssize_t nr;
 	 *
+	 *	// ln can be pre-allocated, and will be reallocated to hold more
 	 *	while ((nr = getline(&ln, &lnsz, stdin)) > 0) {
 	 *		like fgetln(), ln is not a C string,
-	 *		and the last line's last char (ln[nr - 1]) may not be '\n'
+	 *		and the last line's last char (ln[nr - 1]) may not be '\n',
 	 *	}
+	 *	and feof() and ferror() must be checked after the loop
 	 */
 	while ((ln = fgetln(stdin, &nr)) != NULL) {
 		char *nln = NULL;
 
-                if (ln[nr - 1] == '\n') {
+                if (nr && ln[nr - 1] == '\n') {
 			nr--;
 			ln[nr] = '\0';
 		} else {
 	                if (!feof(stdin)) {
-		                perror("fgetln: something funny happened on the way to the farm");
-	                }
+		                perror("stdin: fgetln: something funny happened on the way to the farm");
+	                } else if (ferror(stdin)) {
+		                perror("stdin: fgetln: read error");
+			}
 			/*
 			 * the very last line may not end in a newline, but we
 			 * need room for a NUL to keep strstr() safe
@@ -63,6 +68,7 @@ main(int argc, char *argv[])
 				exit(1);
 			}
 			memcpy(nln, ln, nr);
+			/* xxx could free(ln) here.... */
 			nln[nr] = '\0';
 			ln = nln;
 		}
@@ -79,6 +85,7 @@ main(int argc, char *argv[])
 			nr -= (size_t) (pos - after) + slen;
 			after = pos + slen;
 		}
+		/* xxx n.b. see logic in tsubst.c for partial match at end */
 		if (nr > 0) {
 			if (fwrite(after, (size_t) 1, nr, stdout) != nr) {
 				perror("fwrite(stdout)");
@@ -101,8 +108,9 @@ main(int argc, char *argv[])
 		exit(1);
 	}
 	/*
-	 * XXX the exit() will close and flush stdout, but indication of any
-	 * write error will be lost....  usually that's good enough for unix :-)
+	 * XXX the exit() will freel ln (and nln), and close and flush stdout,
+	 * but indication of any write error will be lost....  usually that's
+	 * good enough for unix :-)
 	 */
 	exit(0);
 }
@@ -110,6 +118,6 @@ main(int argc, char *argv[])
 /*
  * Local Variables:
  * eval: (make-local-variable 'compile-command)
- * compile-command: (let ((fn (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))) (concat "rm " fn "; " (default-value 'compile-command) " " fn " && ./" fn))
+ * compile-command: (let ((fn (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))) (concat "rm " fn "; " (default-value 'compile-command) " " fn " && echo -n 'hello world!' | ./" fn " world reader"))
  * End:
  */
