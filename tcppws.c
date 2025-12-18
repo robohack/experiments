f756c9da3c0f6e2c6eed8b6baffd047b5dc60f9f
Greg A. Woods <woods@robohack.ca>
2025-12-17 18:47:06 -0800

    tcppws.c: add SZLONG, SZCHAR, and SILONG checks

--- tcppws.c
+++ tcppws.c
@@ -6,16 +6,44 @@
 
 #define F2	M(f2)
 
+#define	MKS(x) _MKS(x)
+#define _MKS(x) #x
+
+#define SZLONG	32
+#define SZCHAR	8
+
+#if (SZLONG / SZCHAR) == 2
+# define SILONG 2
+#elif (SZLONG / SZCHAR) == 4
+# define SILONG 4
+#elif (SZLONG / SZCHAR) == 8
+# define SILONG 8
+#else
+# error "Unknown size combinations!"
+#endif
+
 void F2(void);
 
+void
 F2()
 {
-	FILE	*fp;
+	FILE	*fp = stdout;
 
 	lput("");
 	lput("foobar.");
 	lput("nutbar."                        );
 	lput("foobar.");
+	lput(MKS(SZLONG/SZCHAR));
+	lput(MKS(SILONG));
+}
+
+int main(void);
+
+int
+main()
+{
+	F2();
+	return 0;
 }
 
 /*
